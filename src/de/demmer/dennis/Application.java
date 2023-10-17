18c1f82568984ba0cdf9e45732804cbfde34ebbb
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-10-17 11:13:50 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,9 +1,16 @@
 package de.demmer.dennis;
 
 public class Application {
-	
+
+    // window -> show view -> 'Console' auswählen
+    // main -> STRG + LEERTASTE
+	// STRG + SHIFT + F -> auto format
+
+	// Einstiegspunkt in das Programm
 	public static void main(String[] args) {
-			
+		//sysout + STRG + LEERTASTE
+	
+		System.out.println("Hallo Welt");
 	}
 
 }
